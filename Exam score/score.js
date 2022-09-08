43325e63c2ef030c01017cb66b40ebe6757ea16a
dwasit <110547323+dwasit@users.noreply.github.com>
2022-09-08 16:44:07 +0100

    Add files via upload

---
+++ Exam score/score.js
@@ -0,0 +1,7 @@
+const examscore = [34,45,24,56,78,90,100,23,14,54,32,44,64,78,79,78,98,99,89,83,85,68,90,91,39,40,70,86,38]
+
+
+console.log(examscore);
+
+
+if (examscore < 50) { say = "You Failed"} else{ say = "You passed"}
